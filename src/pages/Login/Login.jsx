2f5b4b68e9c90dfaa834807d59bc351915cb858b
Md Monirul Islam <mmislam027@gmail.com>
2022-05-20 22:43:00 +0600

    remove left side from login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import LeftSide from './LeftSide';
 import './Login.css';
 import RightSide from './RightSide';
 
 const Login = () => {
   return (
     <div className='flex min-h-screen items-center bg-gray-100 p-4 lg:justify-center'>
-      <div className='max flex flex-col overflow-hidden rounded-md bg-white shadow-lg md:flex-1 md:flex-row lg:max-w-screen-md'>
-        <LeftSide />
+      <div className='max lg:max-w-screen-xs flex flex-col overflow-hidden rounded-md bg-white shadow-lg md:flex-1 md:flex-row'>
         <RightSide />
       </div>
     </div>
